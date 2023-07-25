aec8ff696c181c947ba985d769e67741441f491e
saeed dabaghian <saeed.dabaghian@gmail.com>
2023-07-25 10:06:17 +0330

    hw10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,68 +1,6 @@
 package org.example;
 
-//import com.sun.xml.internal.ws.api.message.Message;
-//
-//import java.util.Scanner;
-//
-//class Table{
-//    public synchronized void printZoj(int n) {
-//            for (int i = 0; i <= n; i++) {
-//                if (i % 2 == 0) {
-//                    System.out.println(i);
-//                }
-//                try {
-//                    Thread.sleep(n);
-//                } catch (Exception e) {
-//                    Thread.currentThread().interrupt();
-//                    System.out.println(e);
-//                }
-//            }
-//    }
-//    public synchronized void printFard(int n) {
-//            for (int i = 0; i <= n; i++) {
-//                if (i % 2 != 0) {
-//                    System.out.println(i);
-//                }
-//                try {
-//                    Thread.sleep(n);
-//                } catch (Exception e) {
-//                    System.out.println(e);
-//                }
-//            }
-//    }
-//}
-//public class Main {
-//    public static Scanner scanner = new Scanner(System.in);
-//        public static void main(String[] args) {
-//            final Table obj = new Table();
-//            System.out.println("Enter a number: ");
-//            int number = scanner.nextInt();
-//            Thread t1 = new Thread() {
-//                public void run(){
-//                    obj.printFard(number);
-//                }
-//            };
-//            Thread t2 = new Thread(){
-//            public void run(){
-//                obj.printZoj(number);
-//            }
-//            };
-//            t1.start();
-//            t2.start();
-//            }
-//
-//        }
 
-
-
-
-
-
-
-
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
